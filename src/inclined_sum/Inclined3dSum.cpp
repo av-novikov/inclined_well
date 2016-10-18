73c1ee81021cac5c3921bca3900c383e3ce7e0b9
mediev <cranon@rambler.ru>
2016-10-18 14:17:46 +0300

    Gauss boundaries

--- src/inclined_sum/Inclined3dSum.cpp
+++ src/inclined_sum/Inclined3dSum.cpp
@@ -1,6 +1,9 @@
 #include <new>
 
+#include "boost/math/special_functions/gamma.hpp"
 #include "src/inclined_sum/Inclined3dSum.h"
+
+using boost::math::tgamma;
 
 Inclined3dSum::Inclined3dSum(const Parameters* _props, const Well* _well) : BaseSum(_props, _well)
 {
@@ -58,16 +61,19 @@ void Inclined3dSum::prepare3D()
 {
 	double a[2], b[2], c[2], d[2], e[2], f[2];
 	double A, B1, B2;
-	double sum1, sum2;
+	double sum1, sum2, sum3;
 	double buf1, buf2, buf3;
 	int k;
 
-	auto intExpansion = [this](double A, double B) {
-		return 2.0 / sqrt(M_PI) * (1.0 / 2.0 * sqrt(M_PI * B * B / A) * erf(sqrt(A / props->xi_c)) * (2.0 + B * B / A / 3.0 + 3.0 * B * B * B * B / A / A / 20.0) - 
-				B * B * B / A / sqrt(props->xi_c) * exp(-A / props->xi_c) * (1.0 / 3.0 + B * B / props->xi_c / 10.0 + 3.0 * B * B / A / 20.0));
-		/*return 2.0 / sqrt(M_PI) * B / sqrt(props->xi_c) *
-			(2.0 - 2.0 * B * B / 9.0 / props->xi_c * (3.0 * A / B / B + 1.0) +
-			B * B * B * B / 75.0 / props->xi_c / props->xi_c * (15.0 * A * A / B / B / B / B + 10.0 * A / B / B + 3.0));*/
+	auto intExpansion2d = [this](double A, double B) {
+		return -2.0 / sqrt(M_PI) * (pow(B / sqrt(A), 1.5) * tgamma(3.0 / 4.0, A / props->xi_c) * 
+									(A*A - A*B*B / 4.0 + 21.0 / 160.0 * B*B*B*B) + 
+									B*B/A/A / 480.0 * pow(B / sqrt(props->xi_c), 1.5) * exp(-A / sqrt(props->xi_c)) * 
+									(4.0 * A * (3.0 * B*B / sqrt(props->xi_c) - 10) + 21.0 * B*B));
+	};
+	auto intExpansion3d = [this](double A, double B) {
+		return 2.0 / sqrt(M_PI) * (1.0 / 2.0 * sqrt(M_PI * B * B / A) * erf(sqrt(A / props->xi_c)) * (2.0 - B * B / A / 3.0 + 3.0 * B * B * B * B / A / A / 20.0) - 
+				B * B * B / A / sqrt(props->xi_c) * exp(-A / props->xi_c) * (-1.0 / 3.0 + B * B / props->xi_c / 10.0 + 3.0 * B * B / A / 20.0));
 	};
 
 	for (int arr_idx = 0; arr_idx < props->K * props->K; arr_idx++)
@@ -75,7 +81,7 @@ void Inclined3dSum::prepare3D()
 		const WellSegment seg = well->segs[arr_idx % props->K];
 		const Point& point = well->segs[int((double)(arr_idx) / (double)(props->K))].r_bhp;
 
-		F3d[arr_idx] = sum1 = sum2 = 0.0;
+		F3d[arr_idx] = sum1 = sum2 = sum3 = 0.0;
 	
 		for (int p = -props->I; p <= props->I; p++)
 		{
@@ -102,7 +108,7 @@ void Inclined3dSum::prepare3D()
 						(2.0 * sqrt(a[0] * a[0] + c[0] * c[0] + e[0] * e[0]));
 
 					sum1 += sqrt(M_PI / (a[0] * a[0] + c[0] * c[0] + e[0] * e[0])) *
-							(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion(A, B2) - intExpansion(A, B1));
+							(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
 
 					// 0-0-1
 					A = (a[0] * a[0] * (d[0] * d[0] + f[1] * f[1]) -
@@ -116,7 +122,7 @@ void Inclined3dSum::prepare3D()
 						(2.0 * sqrt(a[0] * a[0] + c[0] * c[0] + e[1] * e[1]));
 
 					sum1 += sqrt(M_PI / (a[0] * a[0] + c[0] * c[0] + e[1] * e[1])) *
-						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion(A, B2) - intExpansion(A, B1));
+						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
 
 					// 0-1-0
 					A = (a[0] * a[0] * (d[1] * d[1] + f[0] * f[0]) -
@@ -130,7 +136,7 @@ void Inclined3dSum::prepare3D()
 						(2.0 * sqrt(a[0] * a[0] + c[1] * c[1] + e[0] * e[0]));
 
 					sum1 += sqrt(M_PI / (a[0] * a[0] + c[1] * c[1] + e[0] * e[0])) *
-						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion(A, B2) - intExpansion(A, B1));
+						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
 
 					// 0-1-1
 					A = (a[0] * a[0] * (d[1] * d[1] + f[1] * f[1]) -
@@ -144,7 +150,7 @@ void Inclined3dSum::prepare3D()
 						(2.0 * sqrt(a[0] * a[0] + c[1] * c[1] + e[1] * e[1]));
 
 					sum1 += sqrt(M_PI / (a[0] * a[0] + c[1] * c[1] + e[1] * e[1])) *
-						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion(A, B2) - intExpansion(A, B1));
+						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
 
 					// 1-0-0
 					A = (a[1] * a[1] * (d[0] * d[0] + f[0] * f[0]) -
@@ -158,7 +164,7 @@ void Inclined3dSum::prepare3D()
 						(2.0 * sqrt(a[1] * a[1] + c[0] * c[0] + e[0] * e[0]));
 
 					sum1 += sqrt(M_PI / (a[1] * a[1] + c[0] * c[0] + e[0] * e[0])) *
-						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion(A, B2) - intExpansion(A, B1));
+						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
 
 					// 1-0-1
 					A = (a[1] * a[1] * (d[0] * d[0] + f[1] * f[1]) -
@@ -172,7 +178,7 @@ void Inclined3dSum::prepare3D()
 						(2.0 * sqrt(a[1] * a[1] + c[0] * c[0] + e[1] * e[1]));
 
 					sum1 += sqrt(M_PI / (a[1] * a[1] + c[0] * c[0] + e[1] * e[1])) *
-						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion(A, B2) - intExpansion(A, B1));
+						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
 
 					// 1-1-0
 					A = (a[1] * a[1] * (d[1] * d[1] + f[0] * f[0]) -
@@ -186,7 +192,7 @@ void Inclined3dSum::prepare3D()
 						(2.0 * sqrt(a[1] * a[1] + c[1] * c[1] + e[0] * e[0]));
 
 					sum1 += sqrt(M_PI / (a[1] * a[1] + c[1] * c[1] + e[0] * e[0])) *
-						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion(A, B2) - intExpansion(A, B1));
+						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
 
 					// 1-1-1
 					A = (a[1] * a[1] * (d[1] * d[1] + f[1] * f[1]) -
@@ -200,13 +206,81 @@ void Inclined3dSum::prepare3D()
 						(2.0 * sqrt(a[1] * a[1] + c[1] * c[1] + e[1] * e[1]));
 
 					sum1 += sqrt(M_PI / (a[1] * a[1] + c[1] * c[1] + e[1] * e[1])) *
-						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion(A, B2) - intExpansion(A, B1));
+						(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
 
 				}
 			}
 		}
 
 		sum1 *= (props->visc * props->length / 8.0 / sqrt(M_PI) / M_PI / props->kx);		
+
+		for (int p = -props->I; p <= props->I; p++)
+		{
+			for (int q = -props->I; q <= props->I; q++)
+			{
+				a[0] = props->r1.x - props->r2.x;	b[0] = point.x + 2.0 * (double)(p)* props->sizes.x - props->r1.x;
+				a[1] = props->r2.x - props->r1.x;	b[1] = point.x + 2.0 * (double)(p)* props->sizes.x + props->r1.x;
+				c[0] = props->r1.y - props->r2.y;	d[0] = point.y + 2.0 * (double)(q)* props->sizes.y - props->r1.y;
+				c[1] = props->r2.y - props->r1.y;	d[1] = point.y + 2.0 * (double)(q)* props->sizes.y + props->r1.y;
+
+				// 0-0-0
+				A = (a[0] * a[0] * d[0] * d[0] -
+					2.0 * a[0] * b[0] * c[0] * d[0] +
+					b[0] * b[0] * c[0] * c[0]) /
+					(4.0 * (a[0] * a[0] + c[0] * c[0]));
+				B1 = a[0] * (a[0] * seg.tau1 + b[0]) + c[0] * (c[0] * seg.tau1 + d[0]) + e[0] * (e[0] * seg.tau1 + f[0]) /
+					(2.0 * sqrt(a[0] * a[0] + c[0] * c[0] + e[0] * e[0]));
+				B2 = a[0] * (a[0] * seg.tau2 + b[0]) + c[0] * (c[0] * seg.tau2 + d[0]) + e[0] * (e[0] * seg.tau2 + f[0]) /
+					(2.0 * sqrt(a[0] * a[0] + c[0] * c[0] + e[0] * e[0]));
+
+				sum3 += sqrt(M_PI / (a[0] * a[0] + c[0] * c[0] + e[0] * e[0])) *
+					(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
+
+				// 0-1-0
+				A = (a[0] * a[0] * (d[1] * d[1] + f[0] * f[0]) -
+					2.0 * a[0] * b[0] * (c[1] * d[1] + e[0] * f[0]) +
+					b[0] * b[0] * (c[1] * c[1] + e[0] * e[0]) +
+					(e[0] * d[1] - c[1] * f[0]) * (e[0] * d[1] - c[1] * f[0])) /
+					(4.0 * (a[0] * a[0] + c[1] * c[1] + e[0] * e[0]));
+				B1 = a[0] * (a[0] * seg.tau1 + b[0]) + c[1] * (c[1] * seg.tau1 + d[1]) + e[0] * (e[0] * seg.tau1 + f[0]) /
+					(2.0 * sqrt(a[0] * a[0] + c[1] * c[1] + e[0] * e[0]));
+				B2 = a[0] * (a[0] * seg.tau2 + b[0]) + c[1] * (c[1] * seg.tau2 + d[1]) + e[0] * (e[0] * seg.tau2 + f[0]) /
+					(2.0 * sqrt(a[0] * a[0] + c[1] * c[1] + e[0] * e[0]));
+
+				sum3 += sqrt(M_PI / (a[0] * a[0] + c[1] * c[1] + e[0] * e[0])) *
+					(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
+
+				// 1-0-0
+				A = (a[1] * a[1] * (d[0] * d[0] + f[0] * f[0]) -
+					2.0 * a[1] * b[1] * (c[0] * d[0] + e[0] * f[0]) +
+					b[1] * b[1] * (c[0] * c[0] + e[0] * e[0]) +
+					(e[0] * d[0] - c[0] * f[0]) * (e[0] * d[0] - c[0] * f[0])) /
+					(4.0 * (a[1] * a[1] + c[0] * c[0] + e[0] * e[0]));
+				B1 = a[1] * (a[1] * seg.tau1 + b[1]) + c[0] * (c[0] * seg.tau1 + d[0]) + e[0] * (e[0] * seg.tau1 + f[0]) /
+					(2.0 * sqrt(a[1] * a[1] + c[0] * c[0] + e[0] * e[0]));
+				B2 = a[1] * (a[1] * seg.tau2 + b[1]) + c[0] * (c[0] * seg.tau2 + d[0]) + e[0] * (e[0] * seg.tau2 + f[0]) /
+					(2.0 * sqrt(a[1] * a[1] + c[0] * c[0] + e[0] * e[0]));
+
+				sum3 += sqrt(M_PI / (a[1] * a[1] + c[0] * c[0] + e[0] * e[0])) *
+					(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
+
+				// 1-1-0
+				A = (a[1] * a[1] * (d[1] * d[1] + f[0] * f[0]) -
+					2.0 * a[1] * b[1] * (c[1] * d[1] + e[0] * f[0]) +
+					b[1] * b[1] * (c[1] * c[1] + e[0] * e[0]) +
+					(e[0] * d[1] - c[1] * f[0]) * (e[0] * d[1] - c[1] * f[0])) /
+					(4.0 * (a[1] * a[1] + c[1] * c[1] + e[0] * e[0]));
+				B1 = a[1] * (a[1] * seg.tau1 + b[1]) + c[1] * (c[1] * seg.tau1 + d[1]) + e[0] * (e[0] * seg.tau1 + f[0]) /
+					(2.0 * sqrt(a[1] * a[1] + c[1] * c[1] + e[0] * e[0]));
+				B2 = a[1] * (a[1] * seg.tau2 + b[1]) + c[1] * (c[1] * seg.tau2 + d[1]) + e[0] * (e[0] * seg.tau2 + f[0]) /
+					(2.0 * sqrt(a[1] * a[1] + c[1] * c[1] + e[0] * e[0]));
+
+				sum3 += sqrt(M_PI / (a[1] * a[1] + c[1] * c[1] + e[0] * e[0])) *
+					(asinh(B2 / sqrt(A)) - asinh(B1 / sqrt(A)) + intExpansion3d(A, B2) - intExpansion3d(A, B1));
+			}
+		}
+
+		sum3 *= (props->visc * props->length / 4.0 / M_PI / props->kx);
 
 		for (int m = 1; m <= props->M; m++)
 		{
--- src/inclined_sum/neumann/VerticalNeumannGaussBoundaries.cpp
+++ src/inclined_sum/neumann/VerticalNeumannGaussBoundaries.cpp
@@ -22,9 +22,40 @@ double VerticalNeumannGaussBoundaries::getPres(const Point& point)
 
 double VerticalNeumannGaussBoundaries::getBoundaries(const Point& point) const
 {
-	return 0.0;
-	//return point.x * point.x / 2.0 / props->sizes.x * (props->fx2 - props->fx1) + props->fx1 * point.x +
-	//	point.y * point.y / 2.0 / props->sizes.y * (props->fy2 - props->fy1) + props->fy1 * point.y;
+	double sum = 0.0;
+	
+	sum += point.x * point.x / 2.0 / props->sizes.x * (props->fx2 - props->fx1) + props->fx1 * point.x +
+			point.y * point.y / 2.0 / props->sizes.y * (props->fy2 - props->fy1) + props->fy1 * point.y;
+
+	/*sum += sqrt(2.0 * M_PI * sigma * sigma) / props->sizes.x / props->sizes.y / props->sizes.z * 
+												(erf(props->sizes.y / (2.0 * sqrt(2.0 * sigma * sigma))) * 
+												(point.x * point.x * (Bx2 - Bx1) / 2.0 + props->sizes.x * point.x * Bx1) +
+												erf(props->sizes.x / (2.0 * sqrt(2.0 * sigma * sigma))) *
+												(point.y * point.y * (By2 - By1) / 2.0 + props->sizes.y * point.y * By1));
+
+	double sumX = 0.0;
+	double sumY = 0.0;
+	for (int i = 1; i < props->M; i++)
+	{
+		sumX += cos(2.0 * M_PI * (double)(i)* point.x / props->sizes.x) / (double)(i * i * i) *
+			(By2 * (exp(2.0 * M_PI * (double)(i) * props->sizes.y / props->sizes.x * (point.y / props->sizes.y - 1.0)) + 
+					exp(2.0 * M_PI * (double)(i)* props->sizes.y / props->sizes.x * (-point.y / props->sizes.y - 1.0))) - 
+			By1 * (exp(-2.0 * M_PI * (double)(i) / props->sizes.x * point.y) +
+					exp(2.0 * M_PI * (double)(i)* props->sizes.y / props->sizes.x * (point.y / props->sizes.y - 2.0))));
+
+		sumY += cos(2.0 * M_PI * (double)(i)* point.y / props->sizes.y) / (double)(i * i * i) *
+			(Bx2 * (exp(2.0 * M_PI * (double)(i)* props->sizes.x / props->sizes.y * (point.x / props->sizes.x - 1.0)) +
+					exp(2.0 * M_PI * (double)(i)* props->sizes.x / props->sizes.y * (-point.x / props->sizes.x - 1.0))) -
+			Bx1 * (exp(-2.0 * M_PI * (double)(i) / props->sizes.y * point.x) +
+					exp(2.0 * M_PI * (double)(i)* props->sizes.x / props->sizes.y * (point.x / props->sizes.x - 2.0))));
+	}
+
+	sumX *= (props->sizes.x * props->sizes.x * props->sizes.x / 4 / M_PI / M_PI / M_PI / sigma / sigma);
+	sumY *= (props->sizes.y * props->sizes.y * props->sizes.y / 4 / M_PI / M_PI / M_PI / sigma / sigma);
+
+	sum += sumX + sumY;*/
+
+	return sum;
 }
 
 double VerticalNeumannGaussBoundaries::getPresAvg() const
