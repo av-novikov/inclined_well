96e2b1f085166c2f34d34c375a8268218b4c5191
mediev <cranon@rambler.ru>
2018-03-16 17:47:41 +0300

    Fix zero denominator in InclinedSum3d

--- main.cpp
+++ main.cpp
@@ -78,54 +78,67 @@ void testNeumannGaussBoundary()
 
 	cout << "P_bhp = " << p_bhp << endl;
 	cout << "P_avg = " << p_avg << endl;
-}
+}*/
 void test3D()
 {
-	WellFlow solver("task/config3d.xml");
-	double p_bhp;
+	WellFlow solver("task/config3d_new.xml");
+	auto well = const_cast<Well*>(solver.getWell(0));
+	auto summator = const_cast<BaseSum*>(solver.getSummator(0));
+	const MainProperties* props = solver.getProps();
+	double p_bhp, p1, ccf, ccf_peac, mult;
 
 	auto t = measure_time(
 		[&]() {
-		Inclined3dSum inclSum(solver.getProps(), solver.getWell());
-		solver.setSummator(&inclSum);
-		const Parameters* props = solver.getProps();
-		p_bhp = solver.getP_bhp() * props->p_dim / BAR;
+
+		const double dx = 100.0 / props->x_dim;
+		p_bhp = solver.getP_bhp();
+		auto rc = well->getGeomProps()->rc;
+		p1 = summator->getPressure({ rc.x - dx, rc.y, rc.z });
+		ccf = props->x_dim * props->x_dim * props->x_dim * props->rate * props->visc / (p_bhp - p1 - props->rate * props->visc / 4.0 / props->kx / props->sizes.z) * 86400 * BAR * 1000.0;
+		ccf_peac = props->x_dim * props->x_dim * props->x_dim * 2.0 * M_PI * props->kx * well->getGeomProps()->length / log(0.198 * dx / well->getGeomProps()->rw) * 86400 * BAR * 1000.0;
+		mult = ccf / ccf_peac;
 	}, 1);
 
 	print_test_results("PERF_TEST", t);
-
-	cout << "P_bhp = " << p_bhp << endl;
+	cout << setprecision(4);
+	cout << "P_bhp = \t" << p_bhp * props->p_dim / BAR << endl;
+	cout << "Pi = \t\t" << p1 * props->p_dim / BAR << endl;
+	cout << "CCF = \t\t" << ccf << endl;
+	cout << "CCF_Peaceman = \t" << ccf_peac << endl;
+	cout << "MULT = \t\t" << mult << endl;
 }
-void testDirichlet()
+/*void testDirichlet()
 {
-	WellFlow solver("task/config3d.xml");
+	WellFlow solver("task/horizontal.xml");
+	auto summator = const_cast<BaseSum*>(solver.getSummator(0));
+	auto well = const_cast<Well*>(solver.getWell(0));
 	double p_bhp, p1, p2, p3, p4;
 
 	auto t = measure_time(
 		[&]() {
-		InclinedSum inclSum(solver.getProps(), solver.getWell());
-		solver.setSummator(&inclSum);
+		//InclinedSum inclSum(solver.getProps(), solver.getWell());
+		//solver.setSummator(&inclSum);
+		const MainProperties* props = solver.getProps();
 
-		const Parameters* props = solver.getProps();
-		const auto& rc = props->rc;
-		const double dx = 100.0 / props->x_dim;
+		auto rc = well->getGeomProps()->rc;		rc.y += well->getGeomProps()->rw;
+		const double dx = 0.909 / props->x_dim;
 		
 		p_bhp = solver.getP_bhp() * props->p_dim / BAR;
-		p1 = inclSum.getPres({ rc.x - dx, rc.y, rc.z }) * props->p_dim / BAR;
-		p2 = inclSum.getPres({ rc.x + dx, rc.y, rc.z }) * props->p_dim / BAR;
-		p3 = inclSum.getPres({ rc.x, rc.y - dx, rc.z }) * props->p_dim / BAR;
-		p4 = inclSum.getPres({ rc.x, rc.y + dx, rc.z }) * props->p_dim / BAR;
+		p1 = summator->getPressure({ rc.x - dx, rc.y, rc.z }) * props->p_dim / BAR;
+		p2 = summator->getPressure({ rc.x + dx, rc.y, rc.z }) * props->p_dim / BAR;
+		p3 = summator->getPressure({ rc.x, rc.y - dx, rc.z }) * props->p_dim / BAR;
+		p4 = summator->getPressure({ rc.x, rc.y + dx, rc.z }) * props->p_dim / BAR;
+		well->writeRates(props);
 	}, 1);
 
 	print_test_results("PERF_TEST", t);
-
 	cout << "P_bhp = " << p_bhp << endl;
 	cout << "P_x- = " << p1 << endl;
 	cout << "P_x+ = " << p2 << endl;
 	cout << "P_y- = " << p3 << endl;
 	cout << "P_y+ = " << p4 << endl;
-}
-void testHorizontalLogDerivative()
+}*/
+/*void testHorizontalLogDerivative()
 {
 	WellFlow solver("task/horizontal.xml");
 
@@ -171,25 +184,41 @@ void testFrac2D()
 void testVerticalDirichlet()
 {
 	WellFlow solver("task/vertical.xml");
-	double p_bhp, p_an;
+	double p_bhp, p_an, p1, p2, p3, p4;
+	auto well = const_cast<Well*>(solver.getWell(0));
+	const auto summator1 = static_cast<const VerticalDirichlet*>(solver.getSummator(0));
+	auto summator = const_cast<BaseSum*>(solver.getSummator(0));
+	const MainProperties* props = solver.getProps();
+	const double dx = 100.0 / props->x_dim;
 
 	auto t = measure_time(
 		[&]() {
-		const MainProperties* props = solver.getProps();
 		p_bhp = solver.getP_bhp() * props->p_dim / BAR;
-		p_an = solver.getSummator(0)->getAnalyticalPres() * props->p_dim / BAR;
+		p_an = summator1->getAnalyticalPres(0) * props->p_dim / BAR;
+		auto rc = well->getGeomProps()->rc;		rc.y += well->getGeomProps()->rw;
+		p1 = summator->getPressure({ rc.x - dx, rc.y, rc.z }) * props->p_dim / BAR;
+		p2 = summator->getPressure({ rc.x + dx, rc.y, rc.z }) * props->p_dim / BAR;
+		p3 = summator->getPressure({ rc.x, rc.y - dx, rc.z }) * props->p_dim / BAR;
+		p4 = summator->getPressure({ rc.x, rc.y + dx, rc.z }) * props->p_dim / BAR;
 	}, 1);
 
 	print_test_results("PERF_TEST", t);
 	cout << setprecision(6);
 	cout << "P_bhp = " << p_bhp << endl;
 	cout << "P_ana = " << p_an << endl;
+	cout << "P_x- = " << p1 << endl;
+	cout << "P_x+ = " << p2 << endl;
+	cout << "P_y- = " << p3 << endl;
+	cout << "P_y+ = " << p4 << endl;
 }
 
 int main(int argc, char* argv[])
 {
 	init_paralution();
-	testFrac2D();
+	//testFrac2D();
+	//testDirichlet();
+	//testVerticalDirichlet();
+	test3D();
 	stop_paralution();
 
 	return 0;
--- src/inclined_sum/Inclined3dSum.cpp
+++ src/inclined_sum/Inclined3dSum.cpp
@@ -1,4 +1,5 @@
 #include <new>
+#include <cassert>
 
 #include "src/inclined_sum/Inclined3dSum.h"
 
@@ -12,7 +13,7 @@ double Inclined3dSum::getPressure(const Point& point)
 {
 	double sum = 0.0;
 	double sum_prev_m, sum_prev_n;
-	double buf, F1, F2;
+	double buf, F1, F2, denom1, denom2;
 	int break_idx_m = 0, break_idx_n = 0;
 
 	double sum1 = 0.0, sum2 = 0.0;
@@ -31,32 +32,48 @@ double Inclined3dSum::getPressure(const Point& point)
 			sum_prev_n = 0;	 break_idx_n = 0;
 			for (int n = 1; n <= sprops.M; n++)
 			{
-				F1 = ((sin(M_PI * (double)m * seg.r2.x / props->sizes.x - M_PI * (double)n * seg.r2.y / props->sizes.y) -
-					sin(M_PI * (double)m * seg.r1.x / props->sizes.x - M_PI * (double)n * seg.r1.y / props->sizes.y)) /
-					(M_PI * (double)m / props->sizes.x * rad.x - M_PI * (double)n / props->sizes.y * rad.y) -
-					(sin(M_PI * (double)m * seg.r2.x / props->sizes.x + M_PI * (double)n * seg.r2.y / props->sizes.y) -
-						sin(M_PI * (double)m * seg.r1.x / props->sizes.x + M_PI * (double)n * seg.r1.y / props->sizes.y)) /
-						(M_PI * (double)m / props->sizes.x * rad.x + M_PI * (double)n / props->sizes.y * rad.y)) / 2.0;
+				F1 = 0.0;
+				denom1 = M_PI * (double)m / props->sizes.x * rad.x - M_PI * (double)n / props->sizes.y * rad.y;
+				denom2 = M_PI * (double)m / props->sizes.x * rad.x + M_PI * (double)n / props->sizes.y * rad.y;
+				if (fabs(denom1) < EQUALITY_TOLERANCE)
+					F1 += cos(M_PI * (double)m * seg.r1.x / props->sizes.x - M_PI * (double)n * seg.r1.y / props->sizes.y) * (seg.tau2 - seg.tau1) / 2.0;
+				else
+					F1 += (sin(M_PI * (double)m * seg.r2.x / props->sizes.x - M_PI * (double)n * seg.r2.y / props->sizes.y) -
+						sin(M_PI * (double)m * seg.r1.x / props->sizes.x - M_PI * (double)n * seg.r1.y / props->sizes.y)) / denom1 / 2.0;
+				if (fabs(denom2) < EQUALITY_TOLERANCE)
+					F1 += -cos(M_PI * (double)m * seg.r1.x / props->sizes.x + M_PI * (double)n * seg.r1.y / props->sizes.y) * (seg.tau2 - seg.tau1) / 2.0;
+				else
+					F1 += -(sin(M_PI * (double)m * seg.r2.x / props->sizes.x + M_PI * (double)n * seg.r2.y / props->sizes.y) -
+						sin(M_PI * (double)m * seg.r1.x / props->sizes.x + M_PI * (double)n * seg.r1.y / props->sizes.y)) / denom2 / 2.0;
+
 				buf = M_PI * M_PI * ((double)m * (double)m / props->sizes.x / props->sizes.x + (double)n * (double)n / props->sizes.y / props->sizes.y);
 				f2[arr_idx] += 1.0 / 2.0 * F1 * sin(M_PI * (double)m * r.x / props->sizes.x) *	sin(M_PI * (double)n * r.y / props->sizes.y) * exp(-sprops.xi_c * buf) / buf;
 
 				for (int l = 1; l <= sprops.L; l++)
 				{
-					auto getFoo = [=, this](const Point& pt) -> double
+					auto getFoo = [=, this]() -> double
 					{
-						const Point point = M_PI * pt / props->sizes;
-						const Point p1((double)m, (double)n, -(double)l);
-						const Point p2((double)m, -(double)n, (double)l);
-						const Point p3((double)m, -(double)n, -(double)l);
-						const Point p4((double)m, (double)n, (double)l);
-						return 1.0 / 4.0 * (
-							-sin(p1 * point) / (M_PI * p1 * (rad / props->sizes)) +
-							sin(p2 * point) / (M_PI * p2 * (rad / props->sizes)) +
-							sin(p3 * point) / (M_PI * p3 * (rad / props->sizes)) -
-							sin(p4 * point) / (M_PI * p4 * (rad / props->sizes)));
+						const Point point1 = M_PI * seg.r1 / props->sizes;
+						const Point point2 = M_PI * seg.r2 / props->sizes;
+						const Point p[] = { { (double)m, (double)n, -(double)l },
+											{ (double)m, -(double)n, (double)l },
+											{ (double)m, -(double)n, -(double)l },
+											{ (double)m, (double)n, (double)l } };
+						const double mult[4] = { -1.0, 1.0, 1.0, -1.0 };
+						double denom;
+						double sum = 0.0;
+						for (int i = 0; i < 4; i++)
+						{
+							denom = M_PI * p[i] * (rad / props->sizes);
+							if (fabs(denom) < EQUALITY_TOLERANCE)
+								sum += mult[i] * cos(p[i] * seg.r1) * (seg.tau2 - seg.tau1);
+							else
+								sum += mult[i] * (sin(p[i] * point2) - sin(p[i] * point1)) / denom;
+						}
+						return 1.0 / 4.0 * sum;
 					};
 
-					F2 = getFoo(seg.r2) - getFoo(seg.r1);
+					F2 = getFoo();
 					buf = M_PI * M_PI * ((double)m * (double)m / props->sizes.x / props->sizes.x +
 						(double)n * (double)n / props->sizes.y / props->sizes.y + (double)l * (double)l / props->sizes.z / props->sizes.z);
 					f2[arr_idx] += F2 * sin(M_PI * (double)m * r.x / props->sizes.x) *	sin(M_PI * (double)n * r.y / props->sizes.y) *
@@ -199,7 +216,7 @@ void Inclined3dSum::prepareDirect()
 {
 	double sum = 0.0;
 	double sum_prev_m, sum_prev_n;
-	double buf, F1, F2;
+	double buf, F1, F2, denom1, denom2;
 	int break_idx_m = 0, break_idx_n = 0;
 
 	for (int arr_idx = 0; arr_idx < size; arr_idx++)
@@ -216,65 +233,59 @@ void Inclined3dSum::prepareDirect()
 			sum_prev_n = 0;	 break_idx_n = 0;
 			for (int n = 1; n <= sprops.M; n++)
 			{
-				F1 = ((sin(M_PI * (double)m * seg.r2.x / props->sizes.x - M_PI * (double)n * seg.r2.y / props->sizes.y) -
+				F1 = 0.0;
+				denom1 = M_PI * (double)m / props->sizes.x * rad.x - M_PI * (double)n / props->sizes.y * rad.y;
+				denom2 = M_PI * (double)m / props->sizes.x * rad.x + M_PI * (double)n / props->sizes.y * rad.y;
+				if (fabs(denom1) < EQUALITY_TOLERANCE)
+					F1 += cos(M_PI * (double)m * seg.r1.x / props->sizes.x - M_PI * (double)n * seg.r1.y / props->sizes.y) * (seg.tau2 - seg.tau1) / 2.0;
+				else
+					F1 += (sin(M_PI * (double)m * seg.r2.x / props->sizes.x - M_PI * (double)n * seg.r2.y / props->sizes.y) -
+						sin(M_PI * (double)m * seg.r1.x / props->sizes.x - M_PI * (double)n * seg.r1.y / props->sizes.y)) / denom1 / 2.0;
+				if (fabs(denom2) < EQUALITY_TOLERANCE)
+					F1 += -cos(M_PI * (double)m * seg.r1.x / props->sizes.x + M_PI * (double)n * seg.r1.y / props->sizes.y) * (seg.tau2 - seg.tau1) / 2.0;
+				else 
+					F1 += -(sin(M_PI * (double)m * seg.r2.x / props->sizes.x + M_PI * (double)n * seg.r2.y / props->sizes.y) -
+						sin(M_PI * (double)m * seg.r1.x / props->sizes.x + M_PI * (double)n * seg.r1.y / props->sizes.y)) / denom2 / 2.0;
+
+				/*F1 = ((sin(M_PI * (double)m * seg.r2.x / props->sizes.x - M_PI * (double)n * seg.r2.y / props->sizes.y) -
 						sin(M_PI * (double)m * seg.r1.x / props->sizes.x - M_PI * (double)n * seg.r1.y / props->sizes.y)) /
 						(M_PI * (double)m / props->sizes.x * rad.x - M_PI * (double)n / props->sizes.y * rad.y) -
 					(sin(M_PI * (double)m * seg.r2.x / props->sizes.x + M_PI * (double)n * seg.r2.y / props->sizes.y) -
 						sin(M_PI * (double)m * seg.r1.x / props->sizes.x + M_PI * (double)n * seg.r1.y / props->sizes.y)) /
-						(M_PI * (double)m / props->sizes.x * rad.x + M_PI * (double)n / props->sizes.y * rad.y)) / 2.0;
+						(M_PI * (double)m / props->sizes.x * rad.x + M_PI * (double)n / props->sizes.y * rad.y)) / 2.0;*/
 				buf = M_PI * M_PI * ((double)m * (double)m / props->sizes.x / props->sizes.x + (double)n * (double)n / props->sizes.y / props->sizes.y);
 				F2d[arr_idx] += 1.0 / 2.0 * F1 * sin(M_PI * (double)m * r.x / props->sizes.x) *	sin(M_PI * (double)n * r.y / props->sizes.y) * exp(-sprops.xi_c * buf) / buf;
 
 				for (int l = 1; l <= sprops.L; l++)
 				{
-					auto getFoo = [=, this](const Point& pt) -> double
+					auto getFoo = [=, this]() -> double
 					{
-						const Point point = M_PI * pt / props->sizes;
-						const Point p1((double)m, (double)n, -(double)l);
-						const Point p2((double)m, -(double)n, (double)l);
-						const Point p3((double)m, -(double)n, -(double)l);
-						const Point p4((double)m, (double)n, (double)l);
-						return 1.0 / 4.0 * (
-							-sin(p1 * point) / (M_PI * p1 * (rad / props->sizes)) +
-							sin(p2 * point) / (M_PI * p2 * (rad / props->sizes)) +
-							sin(p3 * point) / (M_PI * p3 * (rad / props->sizes)) -
-							sin(p4 * point) / (M_PI * p4 * (rad / props->sizes)));
+						const Point point1 = M_PI * seg.r1 / props->sizes;
+						const Point point2 = M_PI * seg.r2 / props->sizes;
+						const Point p[] = { { (double)m, (double)n, -(double)l  },
+											{ (double)m, -(double)n, (double)l  },
+											{ (double)m, -(double)n, -(double)l },
+											{ (double)m, (double)n, (double)l	} };
+						const double mult[4] = { -1.0, 1.0, 1.0, -1.0 };
+						double denom;
+						double sum = 0.0;
+						for (int i = 0; i < 4; i++)
+						{
+							denom = M_PI * p[i] * (rad / props->sizes);
+							if (fabs(denom) < EQUALITY_TOLERANCE)
+								sum += mult[i] * cos(p[i] * seg.r1) * (seg.tau2 - seg.tau1);
+							else
+								sum += mult[i] * (sin(p[i] * point2) - sin(p[i] * point1)) / denom;
+						}
+						return 1.0 / 4.0 * sum;
 					};
 
-					F2 = getFoo(seg.r2) - getFoo(seg.r1);
+					F2 = getFoo();
 					buf = M_PI * M_PI * ((double)m * (double)m / props->sizes.x / props->sizes.x +
 						(double)n * (double)n / props->sizes.y / props->sizes.y + (double)l * (double)l / props->sizes.z / props->sizes.z);
 					F2d[arr_idx] += F2 * sin(M_PI * (double)m * r.x / props->sizes.x) *	sin(M_PI * (double)n * r.y / props->sizes.y) *
 						cos(M_PI * (double)l * r.z / props->sizes.z) * exp(-sprops.xi_c * buf) / buf;
 				}
-
-				if (fabs(F2d[arr_idx] - sum_prev_n) > F2d[arr_idx] * EQUALITY_TOLERANCE)
-				{
-					sum_prev_n = F2d[arr_idx];
-					break_idx_n = 0;
-				}
-				else
-					break_idx_n++;
-
-				if (break_idx_n > 1)
-				{
-					//std::cout << m << std::endl;
-					//break;
-				}
-			}
-
-			if (fabs(F2d[arr_idx] - sum_prev_m) > F2d[arr_idx] * EQUALITY_TOLERANCE)
-			{
-				sum_prev_m = F2d[arr_idx];
-				break_idx_m = 0;
-			}
-			else
-				break_idx_m++;
-
-			if (break_idx_m > 1)
-			{
-				//std::cout << m << std::endl;
-				//break;
 			}
 		}
 	}
